39c41c8c75406b7d4e75b17d58d757bf429790ae
Keny SAINT HILAIRE <sainthilaire.keny@gmail.com>
2022-03-21 11:49:04 +0100

    Fix

--- app/src/main/java/fr/airweb/news/MainActivity.kt
+++ app/src/main/java/fr/airweb/news/MainActivity.kt
@@ -2,10 +2,38 @@ package fr.airweb.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import fr.airweb.news.presentation.fragment.NewsListFragmentDirections
+import fr.airweb.news.presentation.fragment.SettingsFragmentDirections
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    fun initView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val nav = findViewById<BottomNavigationView>(R.id.bottomNavigationNew)
+        nav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    navController.navigate(R.id.newsListFragment)
+                }
+
+                R.id.about -> {
+                    navController.navigate(R.id.settingsFragment)
+                }
+
+            }
+            true
+        }
     }
 }
+
--- app/src/main/java/fr/airweb/news/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/fr/airweb/news/presentation/fragment/SettingsFragment.kt
@@ -1,8 +1,10 @@
 package fr.airweb.news.presentation.fragment
 
 
+import android.content.Intent
+import android.net.Uri
 import fr.airweb.news.R
-import fr.airweb.news.databinding.FragmentNewsListBinding
+import fr.airweb.news.databinding.FragmentSettingsBinding
 import fr.airweb.news.presentation.model.SettingsState
 import fr.airweb.news.presentation.viewmodel.SettingsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -11,12 +13,29 @@ class SettingsFragment :
     BaseFragment<
             SettingsState,
             SettingsViewModel,
-            FragmentNewsListBinding>(R.layout.fragment_settings) {
+            FragmentSettingsBinding>(R.layout.fragment_settings) {
 
     override val viewModel: SettingsViewModel by viewModel()
 
-    override fun updateView(state: SettingsState) {
+    override fun updateView(state: SettingsState) {}
+    override fun initView() {
+        with(binding) {
+            contactAddress.setOnClickListener {
+                val address = resources.getString(R.string.contact_address)
+                val url: String = "https://www.google.com/maps/search/?api=1&query=" + address;
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+            contactEmail.setOnClickListener {
+                val email = resources.getString(R.string.contact_email)
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:")
+                intent.putExtra(Intent.EXTRA_EMAIL, email)
+                startActivity(intent)
+            }
+            contactMobile.setOnClickListener {
 
+            }
+        }
     }
-    override fun initView() { }
 }
